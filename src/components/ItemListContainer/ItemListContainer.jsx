eedd10934ed00465704c0818849e053508d5d123
Anthony Cahuin <toni_x159@hotmail.com>
2021-09-11 20:37:49 -0500

    desafio 3

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import ItemList from '../ItemList/ItemList'
+import './ItemListContainer.css'
 
 export const ItemListContainer = ({ salute }) => {
     return (
         <>
-            <h2>Hola { salute } estas son las ofertas de la semana</h2>
+            <h2>Hola { salute } estos son los items que tenemos para ti</h2>
+            <div className="card-container">
+                <ItemList />
+            </div>
         </>
     )
 }
