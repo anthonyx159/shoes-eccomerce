c6f7fab13d3c37066e0322ba406103813ce12139
Anthony Cahuin <toni_x159@hotmail.com>
2021-09-26 00:08:13 -0500

    Sincronizar counter

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import './ItemDetail.css'
+import ItemCount from '../ItemCount/ItemCount'
 
 const ItemDetail = ({prod}) => {
+
+    const onAdd=(cant)=>{
+        console.log(cant)   
+    }  
+
     return (
         <>
             <h2>{prod.title}</h2>
             <h2>{prod.price}</h2>
             <img src={prod.foto} alt="foto" className="detail-product" />
+
+            <ItemCount initial={1} stock={5} onAdd={onAdd} /> 
         </>
     )
 }
